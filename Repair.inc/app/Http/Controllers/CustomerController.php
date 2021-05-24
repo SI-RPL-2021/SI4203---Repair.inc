4b4e741470d84149c46e48bfbd6417fdd9bcfed7
firyalmn <firyalmn22@gmail.com>
2021-05-24 10:10:49 +0700

    Melengkapi pembayaran ref #9

--- Repair.inc/app/Http/Controllers/CustomerController.php
+++ Repair.inc/app/Http/Controllers/CustomerController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Customer;
+use App\Kategori;
+use App\Pesanan;
+use App\Tracking;
+use App\Garansi;
+use App\Feedback;
+use App\Chat;
+use Auth;
 use Illuminate\Support\Facades\Hash;
 
 class CustomerController extends Controller
@@ -11,5 +18,18 @@ class CustomerController extends Controller
 	public function dashboard(){
 
 		return view('auth.customer.dashboard');
+	}
+	public function pembayaran($id){
+		$kategoris = Kategori::limit(4)->get();
+		$pesanan = Pesanan::where('id', $id)->get();
+
+		return view('auth.customer.pembayaran', compact('kategoris','pesanan'));
+}
+	public function proses($id){
+		$kategoris = Kategori::limit(4)->get();
+		$pesanan = Pesanan::where('id', $id)->get();
+		$tracking = Tracking::where('id_pesanan', $id)->get();
+		$selesai_tes = Tracking::where('id_pesanan', $id)->where('status', 'Selesai')->count();
+		return view('auth.customer.proses', compact('kategoris','pesanan','tracking','selesai_tes'));
 	}
 }
--- Repair.inc/app/Http/Controllers/PesananController.php
+++ Repair.inc/app/Http/Controllers/PesananController.php
@@ -45,4 +45,45 @@ class PesananController extends Controller
 
 		return redirect()->back();
 	}
+	public function kirim_bukti(Request $request, $id){
+		$pembayaran = Pembayaran::find($id);
+
+		$pembayaran->id_pesanan = $pembayaran->id_pesanan;
+		$pembayaran->kode_invoice = $pembayaran->kode_invoice;
+		
+		$gambar = $request->file('gambar');
+		$nama_gambar = time() . '_' . '.' . $gambar->getClientOriginalExtension();
+		$tujuan_upload 	= 'img/pembayaran';
+		$gambar->move($tujuan_upload,$nama_gambar);
+		$pembayaran->gambar ='/'.$tujuan_upload.'/'.$nama_gambar;
+
+		$pembayaran->status = 'Proses Verifikasi';
+		$pembayaran->save();
+
+		$tracking = New Tracking();
+		$tracking->id_pesanan = $pembayaran->id_pesanan;
+		$tracking->status = 'Pembayaran';
+		$tracking->save();
+
+		return redirect()->intended('/customer/order');
+	}
+
+	public function konfirmasi_bayar(Request $request){
+		$pembayaran = Pembayaran::find($request->id);
+		$pembayaran->id_pesanan = $pembayaran->id_pesanan;
+		$pembayaran->kode_invoice = $pembayaran->kode_invoice;
+		$pembayaran->status = 'Terbayar';
+		$pembayaran->save();
+
+		$pesanan = Pesanan::find($pembayaran->id_pesanan);
+		$pesanan->status = 'Sedang diproses';
+		$pesanan->save();
+
+		$tracking = New Tracking();
+		$tracking->id_pesanan = $pembayaran->id_pesanan;
+		$tracking->status = 'Proses';
+		$tracking->save();
+
+		return redirect()->back();
+	}
 }
