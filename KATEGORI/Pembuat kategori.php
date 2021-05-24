16d5bfc7791773263dc09210598b9c3dca1315c0
annisanrkhrn <annisanrkhrn@gmail.com>
2021-05-24 10:59:39 +0700

    no message

--- KATEGORI/Pembuat kategori.php
+++ KATEGORI/Pembuat kategori.php
@@ -9,7 +9,7 @@
         <a href="profil.php"> <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/45f8f027-ab1f-4fdd-aa1f-a1eec3a113e4/de4fxss-10c12562-e487-40f4-8972-f0ea54ac59c3.png/v1/fill/w_726,h_816,strp/spongebob_face__meme__by_cmors12_de4fxss-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MTYiLCJwYXRoIjoiXC9mXC80NWY4ZjAyNy1hYjFmLTRmZGQtYWExZi1hMWVlYzNhMTEzZTRcL2RlNGZ4c3MtMTBjMTI1NjItZTQ4Ny00MGY0LTg5NzItZjBlYTU0YWM1OWMzLnBuZyIsIndpZHRoIjoiPD03MjYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.WI7j6SVxPiNbZ8kmFLEztqkiqgvXADoxyhjL85sQ_aM" height="30" alt="no img" class="mr-3"></a>
         <label class=" nav-item dropdown">
           <label class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Yususf
+            
           </label>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Action</a></li>
