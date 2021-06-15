3ece6f78ae0f62367103975305a56a42872851a7
ryansupriadi1 <ryansupriadi1@gmail.com>
2021-06-15 15:57:42 +0700

    Push Tips and Trick atau Artikel ref #3

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@home')->name('home');
 Route::get('/register', 'HomeController@register')->name('home.register')->middleware('guest');
 Route::get('/login', 'HomeController@login')->name('login')->middleware('guest');
-
+Route::get('/artikel', 'HomeController@artikel')->name('artikel');
 Route::post('/login', 'LoginController@postLogin');
 Route::post('/register/post', 'LoginController@register_store')->name('register.store');
 Route::get('/logout', 'LoginController@logout');
-
+Route::get('/artikel/{id}', 'HomeController@artikel_detail')->name('artikel.detail');
 
 // ADMIN
 Route::prefix('admin')->middleware('auth:admin')->group(function () {
@@ -19,6 +19,14 @@ Route::prefix('admin')->middleware('auth:admin')->group(function () {
 		Route::post('/edit/{id}', 'CustomerController@edit')->name('admin.customer.edit');
 		Route::post('/delete/{id}', 'CustomerController@delete')->name('admin.customer.delete');
 });
+
+Route::prefix('artikel')->group(function () {
+		Route::get('/', 'AdminController@artikel')->name('admin.artikel');
+		Route::post('/store', 'ArtikelController@store')->name('admin.artikel.store');
+		Route::post('/edit/{id}', 'ArtikelController@edit')->name('admin.artikel.edit');
+		Route::post('/delete/{id}', 'ArtikelController@delete')->name('admin.artikel.delete');
+	});
+
 });
 
 // CUSTOMER
