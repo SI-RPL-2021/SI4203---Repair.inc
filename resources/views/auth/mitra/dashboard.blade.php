ae445da454e4761e1dbbe8c912992ec1d51f8a56
irgifahrezisalim <irgifahrezyhi@gmail.com>
2021-06-22 13:02:52 +0700

    Push data statistik dari sisi mitra ref #16

--- resources/views/auth/mitra/dashboard.blade.php
+++ resources/views/auth/mitra/dashboard.blade.php
@@ -1,136 +1,152 @@
 @extends('app-dashboard')
 
-@section('title')
-Mitra | Repair.Inch
-@endsection
+@section('title','Mitra | Repair-Inc')
 
 @section('content')
-<div class="page-title">
-	<h3>Dashboard Mitra</h3>
+<div class="page-title mb2">
+  <h3>Dashboard</h3>
 </div>
-<section class="section">
-	<div class="row mb-2">
-		<div class="col-12 col-md-3">
-			<div class="card card-statistic">
-				<div class="card-body p-0">
-					<div class="d-flex flex-column">
-						<div class='px-3 py-3 d-flex justify-content-between'>
-							<h3 class='card-title'>Jumlah Pelanggan</h3>
-							<div class="card-right align-items-center">
-								<p>{{ App\Customer::all()->count() }}</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 col-md-3">
-			<div class="card card-statistic">
-				<div class="card-body p-0">
-					<div class="d-flex flex-column">
-						<div class='px-3 py-3 d-flex justify-content-between'>
-							<h3 class='card-title'>Jumlah Transaksi</h3>
-							<div class="card-right align-items-center">
-								<p>{{ App\Pembayaran::where('status', 'Terbayar')->count() }}</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 col-md-3">
-			<div class="card card-statistic">
-				<div class="card-body p-0">
-					<div class="d-flex flex-column">
-						<div class='px-3 py-3 d-flex justify-content-between'>
-							<h3 class='card-title'>Jasa Mitra</h3>
-							<div class="card-right align-items-center">
-								<p>{{ App\Jasa::all()->count() }}</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 col-md-3">
-			<div class="card card-statistic">
-				<div class="card-body p-0">
-					<div class="d-flex flex-column">
-						<div class='px-3 py-3 justify-content-between'>
-							@php
-							$pembayaran = App\Pembayaran::where('status', 'Terbayar')->get('id_pesanan');
-							$pesanan = App\Pesanan::whereIn('id', $pembayaran)->get();
-							$jumlah = 0;
 
-							foreach($pesanan as $dt) {
-								$jumlahs = App\Jasa::where('id', $dt->id_jasa)->value('harga');
-								$jumlah += $jumlahs;
-							}
-							@endphp
-							<h3 class='card-title'>
-								Pendapatan
-							</h3>
-							<div class="card-right">
-								<p>Rp {{ number_format($jumlah) }} </p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row mb-4">
-		<div class="col-md-7">
-			<div class="card">
-				<div class="card-header">
-					<h3 class='card-heading p-1 pl-3'>Riwayat Pesanan</h3>
-				</div>
-				<div class="card-body">
-					<div class="table-responsive">
-						<table class="table">
-							<thead>
-								<tr>
-									<th>Customer</th>
-									<th>Total Pembayaran Jasa</th>
-									<th>Tanggal</th>
-									<th>Status</th>
-								</tr>
-							</thead>
-							<tbody>
-								@php
-								$pesanan = App\Pesanan::limit(5)->orderBy('created_at', 'desc')->get();
-								@endphp
-								@foreach($pesanan as $ps)
-								<tr>
-									<!-- //customer -->
-									<td>{{ App\Customer::where('id', $ps->id_customer)->value('username') }}</td>
-									<!-- Pembayaran -->
-									<td>{{ App\Jasa::where('id', $ps->id_jasa)->value('harga') }}</td>
-									<!-- Tanggal -->
-									<td>{{ date('d-m-Y', strtotime($ps->created_at)) }}</td>
-									<!-- Status -->
-									<td>{{ $ps->status }}</td>
-								</tr>
-								@endforeach
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-5">
-			<div class="card">
-				<div class="card-header">
-					<h3 class='card-heading p-1 pl-3'>Transaksi</h3>
-				</div>
-				<div class="card-body">
-					<div class="row">
-						<div id="line"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<section class="section">
+  <div class="row mb-2">
+    <div class="col-12 col-md-3">
+      <div class="card card-statistic">
+        <div class="card-body p-0">
+          <div class="d-flex flex-column">
+            <div class='px-3 py-3 d-flex justify-content-between'>
+              <h3 class='card-title'>Sudah Disetujui</h3>
+              <div class="card-right align-items-center">
+
+                <?php
+                $z = [];
+                $count = 0;
+                $count2 = 0;
+                $transaksi = 0;
+                $jumlah = 0;
+                ?>
+                @foreach(App\Jasa::all() as $js)
+                @if($js->id_mitra == Auth::user()->id)
+                <?php
+                array_push($z, $js->id);
+                ?>
+                @endif
+                @endforeach
+
+                @foreach(App\Pesanan::all() as $ps)
+                @if(in_array($ps->id_jasa, $z))
+                <?php
+                $transaksi += 1;
+                ?>
+                @if($ps->status == "Belum diproses" or $ps->status == "Selesai" or $ps->status == "Sedang diproses" or $ps->status == "Terbayar")
+                <?php
+                $count += 1;
+                ?>
+                @foreach(App\Jasa::all() as $js)
+                @if($ps->id_jasa == $js->id)
+                <?php
+                $jumlah += $js->harga;
+                ?>
+                @endif
+                @endforeach
+                @elseif($ps->status == "Belum disetujui" )
+                <?php
+                $count2 += 1;
+                ?>
+                @endif
+                @endif
+                @endforeach
+
+
+
+
+                <p>{{$count}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-3">
+      <div class="card card-statistic">
+        <div class="card-body p-0">
+          <div class="d-flex flex-column">
+            <div class='px-3 py-3 d-flex justify-content-between'>
+              <h3 class='card-title'>Belum Disetujui</h3>
+              <div class="card-right align-items-center">
+                <p>{{ $count2 }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-3">
+      <div class="card card-statistic">
+        <div class="card-body p-0">
+          <div class="d-flex flex-column">
+            <div class='px-3 py-3 d-flex justify-content-between'>
+              <h3 class='card-title'>Transaksi</h3>
+              <div class="card-right align-items-center">
+                <p>{{ $transaksi }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-3">
+      <div class="card card-statistic">
+        <div class="card-body p-0">
+          <div class="d-flex flex-column">
+            <div class='px-3 py-3 justify-content-between'>
+              <h3 class='card-title'>
+                Pendapatan
+              </h3>
+              <div class="card-right">
+                <p>Rp {{ number_format($jumlah) }} </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row mb-4">
+    <!-- <div class="col-md-7"> -->
+    <div class="card">
+      <div class="card-header">
+        <h3 class='card-heading p-1 pl-3'>Pesanan Terbaru</h3>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Customer</th>
+                <th>Jasa</th>
+                <th>Status</th>
+                <th>Tanggal</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach(App\Pesanan::all() as $ps)
+              @if(in_array($ps->id_jasa, $z))
+              <tr>
+                <td>{{ App\Customer::where('id', $ps->id_customer)->value('username') }}</td>
+                <td>{{ App\Jasa::where('id', $ps->id_jasa)->value('nama') }}</td>
+                <td>{{ $ps->status }}</td>
+                <td>{{ date('d-m-Y', strtotime($ps->created_at)) }}</td>
+              </tr>
+              @endif
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- </div> -->
+    </div>
+  </div>
 </section>
 @endsection
 
@@ -138,29 +154,4 @@ Mitra | Repair.Inch
 <script src="{{asset('assets/admin/assets/vendors/dayjs/dayjs.min.js')}}"></script>
 <script src="{{asset('assets/admin/assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
 <script src="{{asset('assets/admin/assets/js/main.js')}}"></script>
-
-<script type="text/javascript">
-	var options = {
-		series: [{{ App\Pesanan::where('status', 'Selesai')->count() }}, {{ App\Pesanan::where('status', 'Ditolak')->count() }}, {{ App\Pesanan::where('status', 'Belum diproses')->count() }}],
-		chart: {
-			width: 380,
-			type: 'pie',
-		},
-		labels: ['Selesai', 'Ditolak', 'Belum Diproses'],
-		responsive: [{
-			breakpoint: 480,
-			options: {
-				chart: {
-					width: 200
-				},
-				legend: {
-					position: 'bottom'
-				}
-			}
-		}]
-	};
-
-	var chart = new ApexCharts(document.querySelector("#line"), options);
-	chart.render();
-</script>
 @endsection
